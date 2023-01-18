5ee9c6df3c8ee109955cc154b02ae2622d6a7891
Bunny Schaefer <SchaeferBunny@gmail.com>
2023-01-18 08:26:43 -0800

    updated auth settings

--- src/components/AuthWrapper/AuthWrapper.js
+++ src/components/AuthWrapper/AuthWrapper.js
@@ -4,8 +4,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 export const AuthWrapper = ({ children }) => {
   const curOrganization = localStorage.getItem("organization_id");
   const options = {
-    domain: "dev-rm3u1yq4.auth0.com",
-    clientId: "OgdvG5snvJGpeIrW9FKXbB5D9UmSQufP",
+    domain: "dev-xpa4ghv8.us.auth0.com",
+    clientId: "aWYaHAbljpuOAg4t2dafPV8HYAtLisX6",
     redirectUri: window.location.origin,
     ...(curOrganization ? { organization: curOrganization } : null)
   };
